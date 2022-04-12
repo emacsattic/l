4cad5aef534c8a731b852ed6fe04654e233114f0
Jonas Bernoulli <jonas@bernoul.li>
2022-04-12 16:18:34 +0200

    Refresh library header

--- l.el
+++ l.el
@@ -1,24 +1,25 @@
 ;;; l.el --- Compact syntax for short lambda       -*- lexical-binding: t -*-
 
-;; Copyright (C) 2021  Jonas Bernoulli
+;; Copyright (C) 2021-2022 Jonas Bernoulli
 
 ;; Authors: Jonas Bernoulli <jonas@bernoul.li>
-;; URL: https://git.sr.ht/~tarsius/l
+;; Homepage: https://git.sr.ht/~tarsius/l
 ;; Keywords: extensions
 
 ;; Package-Requires: ((seq "2.20"))
+
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published
 ;; by the Free Software Foundation, either version 3 of the License,
 ;; or (at your option) any later version.
-
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
